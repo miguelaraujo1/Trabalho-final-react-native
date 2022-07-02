4a60713d242e854862d05b07fcc54d5c7f4110a6
Paulo <pvpmartins@hotmail.com>
2022-07-02 18:57:59 -0300

    styles.card added

--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -4,6 +4,12 @@ export const styles = StyleSheet.create({
     container: {
         flex:1,
         alignItems: 'center',
-        justifyContent: 'center'
-    }
+        marginTop: 30
+    },
+    item: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+      },
 })
