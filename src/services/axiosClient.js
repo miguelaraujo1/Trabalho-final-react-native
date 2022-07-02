257b9e405d8c1ad769d67d5734dab1553d0bc4b3
Paulo <pvpmartins@hotmail.com>
2022-07-02 18:57:18 -0300

    arquivo com funcoes de axios criado

---
+++ src/services/axiosClient.js
@@ -0,0 +1,13 @@
+import api from "./api";
+
+export const getProdutos = async() => {
+    const { data } = await api.get("/tegloja/produtos")
+
+    return data
+}
+
+export const getProdutosByCategoria = async (id) => {
+    const { data } = await api.get(`/tegloja/categorias/${id}/produtos`)
+
+    return data
+}
