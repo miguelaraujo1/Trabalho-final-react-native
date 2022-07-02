c3ec9842a6c8dae1c34f71f5336acbeccc9dad86
Paulo <pvpmartins@hotmail.com>
2022-07-02 18:56:11 -0300

    flatList implementado

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,40 @@
-import React from "react"
-import { ImageBackground, Text } from "react-native"
+import React, { useEffect, useState } from "react"
+import { ImageBackground, Text, FlatList} from "react-native"
+import { getProdutos } from "../../services/axiosClient"
 import { styles } from "./styles"
 
+const Item = ({ title }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+);
+
 const Home = () => {
+    const [listaProdutos, setListaProdutos] = useState(null)
+    
+    const listarProdutos = async () => {
+        const produtos = await getProdutos()
+        setListaProdutos(produtos)
+    }
+
+    useEffect(()=> {
+        listarProdutos()
+    },[])
+
+    useEffect(()=>{
+        console.log(listaProdutos)
+    },[listaProdutos])
+
+    // const renderItem = ({ item }) => (
+    //     <Item title={item.nomeProduto} />
+    // );
+
     return (
         <ImageBackground style={styles.container}>
             <Text>BikeLovers!</Text>
+            {listaProdutos && <FlatList data={listaProdutos} renderItem={({ item })=> (
+                <Text style={styles.item}>{item.nomeProduto}</Text>
+            )} keyExtractor={item => item.idProduto}/>}
         </ImageBackground>
     )
 }
