fa12c93798f512250542f7d033d6ab1c76fd45f8
Paulo <pvpmartins@hotmail.com>
2022-07-02 00:53:40 -0300

    Arquivo de rotas criado

---
+++ src/Routes/index.js
@@ -0,0 +1,21 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "../screens/Home";
+
+const Stack = createNativeStackNavigator()
+
+const Routes = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name='Home'
+                    component={Home}
+                    options={{headerShown: false}}
+                    />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default Routes
