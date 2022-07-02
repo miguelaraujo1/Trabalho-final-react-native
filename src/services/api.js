3a885ecbda79b0332d2c921b0ab968510efc3332
Paulo <pvpmartins@hotmail.com>
2022-07-02 17:51:36 -0300

    export default api added

--- src/services/api.js
+++ src/services/api.js
@@ -4,5 +4,8 @@ const api = axios.create({
     baseURL: "https://tegloja-api.herokuapp.com/"
 })
 
+export default api
+
 // Documentação Swagger:
 // tegloja-api.herokuapp.com/swagger-ui/index.html#/
+
