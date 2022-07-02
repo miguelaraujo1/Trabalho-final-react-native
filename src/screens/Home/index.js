0b543fdb7a9e7c6e1923894e771e91281b49c21f
Paulo <pvpmartins@hotmail.com>
2022-07-02 00:54:50 -0300

    Home criada

---
+++ src/screens/Home/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { ImageBackground, Text } from "react-native"
+import { styles } from "./styles"
+
+const Home = () => {
+    return (
+        <ImageBackground style={styles.container}>
+            <Text>BikeLovers!</Text>
+        </ImageBackground>
+    )
+}
+
+export default Home
