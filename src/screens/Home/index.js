415a58a0e1ee020b7607e14c089dfe5b6a798a16
Paulo <pvpmartins@hotmail.com>
2022-07-02 22:39:52 -0300

    home listando produtos por categoria

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react"
-import { ImageBackground, Text, FlatList} from "react-native"
-import { getProdutos } from "../../services/axiosClient"
+import { ImageBackground, Text, FlatList, View} from "react-native"
+import { Button} from 'react-native-paper';
+import { getCategorias, getProdutos, getProdutosByCategoria } from "../../services/axiosClient"
 import { styles } from "./styles"
 
 const Item = ({ title }) => (
@@ -11,19 +12,35 @@ const Item = ({ title }) => (
 
 const Home = () => {
     const [listaProdutos, setListaProdutos] = useState(null)
+    const [categorias, setCategorias] = useState(null)
     
     const listarProdutos = async () => {
         const produtos = await getProdutos()
         setListaProdutos(produtos)
     }
 
+    const listarProdutosPorCategoria = async(id) => {
+        const produtos = await getProdutosByCategoria(id)
+        setListaProdutos(produtos)
+    }
+
+    const listarCategorias = async () => {
+        const categorias = await getCategorias()
+        setCategorias(categorias)
+    }
+
     useEffect(()=> {
-        listarProdutos()
+        if(listaProdutos === null) {
+            listarProdutos()
+        }
+        listarCategorias()
     },[])
 
     useEffect(()=>{
-        console.log(listaProdutos)
+        // console.log(listaProdutos)
     },[listaProdutos])
+
+    useEffect(()=>{console.log(categorias)},[categorias])
 
     // const renderItem = ({ item }) => (
     //     <Item title={item.nomeProduto} />
@@ -32,6 +49,9 @@ const Home = () => {
     return (
         <ImageBackground style={styles.container}>
             <Text>BikeLovers!</Text>
+            <View style={styles.categorias}>
+            {categorias && categorias.map(categoria => <Button mode="contained" key={categoria.id}  onPress={()=>listarProdutosPorCategoria(categoria.id)}>{categoria.categoria}</Button>)}
+            </View>
             {listaProdutos && <FlatList data={listaProdutos} renderItem={({ item })=> (
                 <Text style={styles.item}>{item.nomeProduto}</Text>
             )} keyExtractor={item => item.idProduto}/>}
