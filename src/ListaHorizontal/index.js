4bd69c3a2b471dc9f8b5de97a331314465407899
Miguel Araujo <miguelaraujo2002@gmail.com>
2022-07-03 16:07:06 -0300

    fotos arredondadas

--- src/ListaHorizontal/index.js
+++ src/ListaHorizontal/index.js
@@ -2,34 +2,29 @@ import React from "react";
 import { Dimensions, FlatList } from "react-native";
 import { View, Image, Text } from "react-native";
 import { ImageBackground } from "react-native";
+import {styles} from "./styles"
 
-const { width } = Dimensions.get("window");
+
 const ListaHorizontal = ({ dados }) => {
   return (
+    <View style={{}}>
     <FlatList
       data={dados}
       keyExtractor={(item) => item.id}
       horizontal
       renderItem={({ item }) => 
       
-      <View 
-          style={{
-            // backgroundColor: item.cor,
-            width: width * 0.8,
-            height: 600,
-            marginHorizontal: 10,
-            borderRadius: 12,
-            flex:1
-          }}
-        >
-             <Text style={{alignItems:"center",justifyContent:"center",fontSize:50, fontWeight:"bold"}}>{item.name}</Text>
-             <ImageBackground style={{width:'100%', height:'100%'}} source={item.foto}><View style={{width:100,height:100,flex:0.6}}></View></ImageBackground>
+      <View style={styles.flatList}>
+
+             <ImageBackground style={styles.fotos} source={item.foto}><View style={{width:50,height:50}}></View></ImageBackground>
+             <Text style={{height:100 ,alignItems:"center",justifyContent:"center",fontSize:50, fontWeight:"bold"}}>{item.name}</Text>
              
         </View>
        
         
       }
     />
+    </View>
   );
 };
 export default ListaHorizontal;
---
+++ src/ListaHorizontal/styles.js
@@ -0,0 +1,24 @@
+import { StyleSheet, Dimensions } from "react-native";
+const { width } = Dimensions.get("window");
+export const styles = StyleSheet.create({
+    flatList:{
+        width: width * 0.8,
+        height: 600,
+        marginHorizontal: 10,
+        // borderRadius: 12,
+        flex:1,
+        marginTop:40,
+        alignItems:"center",
+        backgroundColor:"grey"
+    },
+    fotos:{
+        width: 150,
+        height: 150,
+        borderRadius: 150 / 2,
+        overflow: "hidden",
+        borderWidth: 3,
+        borderColor: '#add8e6',
+        marginTop:12
+    }
+}
+)
