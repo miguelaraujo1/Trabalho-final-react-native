abc89e722b07d8087c624fc367bb0116d58eee8a
Miguel Araujo <miguelaraujo2002@gmail.com>
2022-07-03 03:12:24 -0300

    flatlist horizontal já com fotos, falta estilizar

--- src/ListaHorizontal/index.js
+++ src/ListaHorizontal/index.js
@@ -1,33 +1,35 @@
 import React from "react";
 import { Dimensions, FlatList } from "react-native";
-import { View, Image } from "react-native";
+import { View, Image, Text } from "react-native";
+import { ImageBackground } from "react-native";
 import bgImg from "../images/will.jfif";
 
 const { width } = Dimensions.get("window");
-const ListaHorizontal = ({ data }) => {
+const ListaHorizontal = ({ dados }) => {
   return (
     <FlatList
-      data={data}
-      keyExtractor={(item) => String(item)}
+      data={dados}
+      keyExtractor={(item) => item.id}
       horizontal
-      renderItem={(item) => (
-        <View>
-        <View
+      renderItem={({ item }) => 
+      
+      <View 
           style={{
-            backgroundColor: "green",
+            // backgroundColor: item.cor,
             width: width * 0.8,
             height: 600,
             marginHorizontal: 10,
             borderRadius: 12,
+            flex:1
           }}
         >
-          <Image
-            style={{ width: 50, height: 60 }}
-            source={{ uri: bgImg }}
-          ></Image>
+             <Text style={{alignItems:"center",justifyContent:"center",fontSize:50, fontWeight:"bold"}}>{item.name}</Text>
+             <ImageBackground style={{width:'100%', height:'100%'}} source={item.foto}><View style={{width:100,height:100,flex:0.6}}></View></ImageBackground>
+             
         </View>
-        </View>
-      )}
+       
+        
+      }
     />
   );
 };
--- src/screens/QuemSomos/index.js
+++ src/screens/QuemSomos/index.js
@@ -1,22 +1,59 @@
 import React from "react";
 import { SafeAreaView } from "react-native";
 import { View, Image } from "react-native";
-import bgImg from "../../images/will.jfif"
+import bgImg from "../../images/will.jfif";
 import ListaHorizontal from "../../ListaHorizontal";
 
+const will = require("../../images/will.jfif");
+const miguel = require("../../images/miguel.jpeg");
+const danilo = require("../../images/Danilo.jpeg");
+const eduardo = require("../../images/eduardo.jpg");
+const vinicius = require("../../images/vinicius.jpg");
+const cynthia = require("../../images/cynthia.jpg");
+
 const QuemSomos = () => {
-  const data = [
-    '#FF6633',
-    '#FFB399',
-    '#FF6667',
-    '#FF6111',
-    '#FF6444',
-    
+  const dados = [
+    {
+      cor: "#F12",
+      name: "Miguel",
+      foto: miguel,
+      id: "1",
+    },
+    {
+      cor: "#F20",
+      name: "Danilo",
+      foto: danilo,
+      id: "2",
+    },
+    {
+      cor: "#F20",
+      name: "Pv",
+      foto: will,
+      id: "3",
+    },
+    {
+      cor: "#F20",
+      name: "Cynthia",
+      foto: cynthia,
+      id: "4",
+    },
+    {
+      cor: "#F20",
+      name: "Vinicius",
+      foto: vinicius,
+      id: "5",
+    },
+    {
+        cor:'#F20',
+        name:'Eduardo',
+        foto: eduardo,
+        id:"6"
+    },
   ];
-  return(
+  return (
     <SafeAreaView>
-        <ListaHorizontal data={data}/>
+      <ListaHorizontal dados={dados} />
     </SafeAreaView>
-  )
+  );
 };
- export default QuemSomos;
+export default QuemSomos;
