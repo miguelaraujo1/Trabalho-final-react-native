b514124b4cd97996a5ca81c24f23a99b2a58c0c4
Paulo <pvpmartins@hotmail.com>
2022-07-04 02:10:27 -0300

    cadastro criado

--- src/screens/Produtos/CadastrarProdutos/index.js
+++ src/screens/Produtos/CadastrarProdutos/index.js
@@ -2,38 +2,72 @@ import React, { useEffect, useState } from "react";
 import { launchImageLibrary } from "react-native-image-picker";
 import { getCategorias, postProduto, getProdutos } from "../../../services/axiosClient";
 import SelectPicker from 'react-native-form-select-picker';
-
+import { Text, View} from 'react-native'
+import { Picker } from "@react-native-picker/picker";
+import { Button, TextInput } from "react-native-paper";
+import { styles } from "./styles";
 const CadastrarProdutos = ({CadastrarProdutos}) => {
-    const [selected, setSelected] = useState();
-    const [listaProdutos, setListaProdutos] = useState(null)
+    const [categoria, setCategoria] = useState();
+    const [listaCategorias, setListaCategorias] = useState(null)
+    const [nomeProduto, setNomeProduto] = useState()
+    const [quantidade, setQuantidade] = useState()
+    const [valorUnitario, setValorUnitario] = useState()
+    const [warning, setWarning] = useState()
 
-    const listarProdutos = async () => {
-        const produtos = await getProdutos()
-        setListaProdutos(produtos)
+    const listarCategorias = async () => {
+        const categorias = await getCategorias()
+        setListaCategorias(categorias)
     }
 
     useEffect(()=>{
-        listarProdutos()
+        listarCategorias()
     },[])
 
+    useEffect(()=>{
+        console.log(typeof categoria)
+    },[categoria])
+
+    const inserirProduto = async() => {
+        console.log(typeof categoria.id, typeof nomeProduto, typeof quantidade, typeof valorUnitario)
+        if (!!categoria && !!nomeProduto && quantidade && valorUnitario){
+            const post = await postProduto(categoria.id ,nomeProduto, quantidade, valorUnitario)
+            console.log(post)
+        } else {
+            setWarning('Campo(s) nao preenchidos')
+        }
+
+    }
+
+    
 
 
 	return (
 		//...
         <>
-             <SelectPicker
-                onValueChange={(value) => {
-                    setSelected(value);
-                }}
-                selected={selected}
-                >
-                
-                {listaProdutos && Object.values(listaProdutos).map((val, index) => (
-                    <SelectPicker.Item label={val} value={val} key={index} />
-                    ))}
+        <View  style={{alignItems:'center', marginTop: 20}}>
+            <Text >Escolha a categoria</Text>
+        </View>
+        <Picker selectedValue = {categoria} onValueChange = {(value) => setCategoria(value)}>
+            {listaCategorias && listaCategorias.map((categoria, index) => <Picker.Item key={index} label={categoria.categoria} value={categoria} />)}     
+        </Picker>
+        <View style={styles.field}>
+            <Text>Insira o nome do produto:</Text>
+            <TextInput style={styles.input} mode='outlined' value={nomeProduto} onChangeText={(text) => setNomeProduto(text)} />
+        </View>
+        <View style={[styles.field, {marginTop: 10}]}>
+            <Text>Insira a quantidade para estoque</Text>
+            <TextInput style={styles.input} mode='outlined' value={quantidade} onChangeText={(text) => setQuantidade(text)} />
+        </View>
+        <View style={[styles.field, {marginTop: 10, marginBottom: 15}]}>
+            <Text>Insira o Valor unitario - R$0,00</Text>
+            <TextInput style={styles.input} mode='outlined' value={valorUnitario} onChangeText={(text) => setValorUnitario(text)} />
+        </View>
 
-            </SelectPicker>
+        {!!warning && warning}
+
+        <Button mode='contained-tonal' onPress={inserirProduto}>CADASTRAR</Button>
         </>
+        
 
 	)
 }
