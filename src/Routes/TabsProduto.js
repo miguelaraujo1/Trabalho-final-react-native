e8ab125bca941b25a1f0ab7139693daa714e220b
Paulo <pvpmartins@hotmail.com>
2022-07-05 11:54:27 -0300

    Componente renomeado para TabsProduto

---
+++ src/Routes/TabsProduto.js
@@ -0,0 +1,22 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import ListarProdutos from "../screens/Produtos";
+import CadastrarProdutos from "../screens/Produtos/CadastrarProdutos";
+import ExibirProduto from "../screens/Produtos/ExibirProduto";
+
+
+const Tabs = createBottomTabNavigator()
+
+const TabsProduto = () => {
+
+    return(
+        <>
+        <Tabs.Navigator>
+            <Tabs.Screen name="Listar" component={ListarProdutos} options={{headerShown: false}}/>
+            <Tabs.Screen name="Cadastrar" component={CadastrarProdutos} options={{headerShown: false}}/>
+        </Tabs.Navigator>
+        </>
+    )
+}
+
+export default TabsProduto
